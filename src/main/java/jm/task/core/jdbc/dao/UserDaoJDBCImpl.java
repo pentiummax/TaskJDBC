2876e3ed69fa66278dd5bb9d10f97bd8f24616c7
Robert <ptnca47@gmail.com>
2020-11-24 20:00:55 +0300

    statement -> preparedStatement

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -4,10 +4,7 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement();) {
+        try (Statement statement = connection.createStatement()) {
             connection.setAutoCommit(false);
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS Users " +
                     "(id BIGINT PRIMARY KEY AUTO_INCREMENT, " +
@@ -71,10 +68,13 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Statement statement = connection.createStatement()) {
+        try (PreparedStatement statement = connection.prepareStatement("INSERT Users(name, lastName, age) " +
+                "VALUES (?, ?, ?)")) {
             connection.setAutoCommit(false);
-            statement.executeUpdate("INSERT Users(name, lastName, age) " +
-                    "VALUES ('" + name + "', '" + lastName + "', " + age + ")");
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
             connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -93,9 +93,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (Statement statement = connection.createStatement()) {
+        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM Users WHERE id = ?")) {
             connection.setAutoCommit(false);
-            statement.executeUpdate("DELETE FROM Users WHERE id = " + id);
+            statement.setLong(1, id);
+            statement.executeUpdate();
             connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
